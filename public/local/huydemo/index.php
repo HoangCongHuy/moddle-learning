00186424b3eda213969b7460ed069d04e9f0cd43
HoangCongHuy <hoangconghuy97@gmail.com>
2026-03-24 01:57:23 +0700

    feat: Add task deletion functionality and refactor task list display using a Mustache template.

--- public/local/huydemo/index.php
+++ public/local/huydemo/index.php
@@ -1,15 +1,16 @@
 <?php
 
-use core\output\html_writer;
-use core\output\notification;
-use local_huydemo\form\task_form;
-
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/local/huydemo/classes/form/task_form.php');
 
+use core\output\html_writer;
+use core\output\notification;
+use core\url;
+use local_huydemo\form\task_form;
+
 global $DB, $USER, $PAGE, $OUTPUT;
 
-$url = new moodle_url('/local/huydemo/index.php');
+$url = new url('/local/huydemo/index.php');
 $PAGE->set_url($url);
 $PAGE->set_context(context_system::instance());
 $PAGE->set_title('Manager Task');
@@ -31,26 +32,42 @@ if ($mform->is_cancelled()) {
     redirect(url: $url, message: 'Task added successfully', messagetype: notification::NOTIFY_SUCCESS);
 }
 
+$action = optional_param(parname: 'action', default: '', type: PARAM_ALPHA);
+$taskid = optional_param(parname: 'taskid', default: 0, type: PARAM_INT);
+
+if ($action === 'delete' && $taskid > 0) {
+    require_sesskey();
+
+    $DB->delete_records('local_huydemo_tasks', ['id' => $taskid, 'userid' => $USER->id]);
+    redirect(url: $url, message: 'Task deleted successfully', messagetype: notification::NOTIFY_SUCCESS);
+}
+
 echo $OUTPUT->header();
-
 $mform->display();
-
 echo html_writer::tag(tagname: 'hr', contents: '');
-
-echo html_writer::tag(tagname: 'h3', contents: 'List tasks');
 
 $tasks = $DB->get_records('local_huydemo_tasks', ['userid' => $USER->id], 'timecreated DESC');
 
-if ($tasks) {
-    echo html_writer::start_tag(tagname: 'ul');
-    foreach ($tasks as $task) {
-        $date = userdate(date: $task->timecreated);
-        echo html_writer::tag(tagname: 'li', contents: "{$task->taskname} (Created at: $date)");
-    }
+$templatedata = new stdClass();
+$templatedata->hastasks = !empty($tasks);
+$templatedata->tasks = [];
 
-    echo html_writer::end_tag(tagname: 'ul');
-} else {
-    echo html_writer::tag(tagname: 'p', contents: 'No tasks');
+if ($templatedata->hastasks) {
+    foreach ($tasks as $task) {
+        $deleteUrl = new url('/local/huydemo/index.php', [
+            'action' => 'delete',
+            'taskid' => $task->id,
+            'sesskey' => sesskey(),
+        ]);
+
+        $templatedata->tasks[] = [
+            'taskname' => $task->taskname,
+            'date' => userdate($task->timecreated),
+            'deleteurl' => $deleteUrl->out(false),
+        ];
+    }
 }
+
+echo $OUTPUT->render_from_template('local_huydemo/task_list', $templatedata);
 
 echo $OUTPUT->footer();
